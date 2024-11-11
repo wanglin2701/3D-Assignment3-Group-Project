cadb170f8f793e6b55b7cb262f71b92c631c9878
MrJit-afk <jscheong2000@gmail.com>
2024-11-11 19:02:12 +0800

    Basic Door & Door keypad implemented

--- Assets/Scripts/interactables/Keypad.cs
+++ Assets/Scripts/interactables/Keypad.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Keypad : Interactable
 {
+    [SerializeField]
+    private GameObject door;
+    private bool doorOpen;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,7 @@ public class Keypad : Interactable
     //this function is where the interaction will be designed using code
     protected override void Interact()
     {
-        Debug.Log("Interacted with" + gameObject.name);
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("IsOpen", doorOpen);
     }
 }
