a78923d303cc10039a834925911bae2632eba8ae
MrJit-afk <jscheong2000@gmail.com>
2024-11-11 20:44:24 +0800

    Damage Overlay implemented

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,9 +5,19 @@ using UnityEngine.UIElements;
 
 public abstract class Interactable : MonoBehaviour
 {
+    //Add or remove an InteractionEvent component to this gameObject
+    public bool useEvents;
+    [SerializeField]
     public string promptMessage;
+
+    public virtual string OnLook()
+    {
+        return promptMessage;
+    }
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
     protected virtual void Interact()
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.UIElements;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    private float lerpTimer;
+
+    [Header("Health Bar")]
+    public float maxHealth = 100;
+    public float chipSpeed = 2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+
+    [Header("Damage Overlay")]
+    public Image overlay; // the reddy frame
+    public float duration; // how long the frame stays fully opaque
+    public float fadeSpeed; // how quickly the frame will fade
+
+    private float durationTimer; // timer to check againts the duration
+
+    
+    void Start()
+    {
+        health = maxHealth;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        if(overlay.color.a > 0)
+        {
+            
+            // if the health drop below 30, the red frame stays!
+            if(health < 30)
+            {
+                return;
+            }
+            durationTimer += Time.deltaTime;
+            if(durationTimer > duration)
+            {
+                //fade the image
+                float tempAlpha = overlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, tempAlpha);
+            }
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if (fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if(fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+        durationTimer = 0;
+        overlay.color = new Color(overlay.color.r, overlay.color.g, overlay.color.b, 1);
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+}
