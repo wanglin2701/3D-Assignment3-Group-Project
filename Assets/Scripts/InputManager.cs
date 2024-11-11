4b3b8e70f60eb4027dde73f32e2fa19f73c1f994
MrJit-afk <jscheong2000@gmail.com>
2024-11-11 16:41:27 +0800

    Basic movements added

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+    private PlayerMotor motor;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();
+        onFoot.Jump.performed += ctx => motor.Jump();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //tell the playermotor to mvoe using the value from our movement action
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
